cb97dc19e9a6aec99a83ad45dc1be076a0ecad14
Chihao Feng <fengchihao@gmail.com>
2018-12-28 18:32:20 -0500

    build the checkout container and set up routing and routes

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
-
+import { withRouter } from 'react-router-dom';
 const burger = (props) => {
+  console.log(props);
   let transformedIngredients = Object.keys(props.ingredients).map(igkey => {
     return [...Array(props.ingredients[igkey])].map((_, i) => {
       return <BurgerIngredient key={igkey + i} type={igkey}/>
@@ -35,4 +36,4 @@ const burger = (props) => {
   );
 };
 
-export default burger;
+export default withRouter(burger);
