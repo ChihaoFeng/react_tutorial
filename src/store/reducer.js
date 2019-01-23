dd14eaaa35ff56ecedf3329c36eda45948f3c2f1
Chihao Feng <fengchihao@gmail.com>
2019-01-23 17:36:36 -0500

    work on total price calculation

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,6 +10,14 @@ const initialState = {
   totalPrice: 4
 };
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+
+};
+
 const reducer = (state = initialState, actions) => {
   switch (actions.type) {
     case actionTypes.ADD_INGREDIENT:
@@ -18,7 +26,8 @@ const reducer = (state = initialState, actions) => {
         ingredients:{
           ...state.ingredients,
           [actions.ingredientName]: state.ingredients[actions.ingredientName] + 1
-        }
+        },
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[actions.ingredientName]
       };
     case actionTypes.REMOVE_INGREDIENT:
       return {
@@ -26,7 +35,8 @@ const reducer = (state = initialState, actions) => {
         ingredients:{
           ...state.ingredients,
           [actions.ingredientName]: state.ingredients[actions.ingredientName] - 1
-        }
+        },
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[actions.ingredientName]
       };
     default:
       return state;
